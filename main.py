92ba490f206a53a5aebbec12509afbcb424ecb36
Vigor Turujlija <vigor.turujlija@gmail.com>
2019-12-18 23:34:38 +0100

    Created logins

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 
+
+root = tk.Tk()
+root.title("Mail Styler")
+selectedLogin = tk.StringVar(root)
+
 def generateHtml(title, jsonStr):
     jsonvar = json.loads(jsonStr)
     shellHtml = open('customMailShell.html', "r").read()
@@ -32,10 +37,13 @@ def generateList():
     contentadder = list('')
     subtitleadder = list('')
     idx=0
+    error = False
+    lastOpenSubtitle = 0
     while idx < len(text):
         char = text[idx]
         # print(char)
         if char == '<':
+            lastOpenSubtitle = idx
             if contentadder != list(''):
                 jsonvar.append({'type':'cont', 'content':''.join(contentadder)})
                 contentadder = list('')
@@ -44,7 +52,10 @@ def generateList():
             while idx < len(text) and running:
                 idx += 1
                 char = text[idx]
-                if char == '>':
+                if idx >= len(text) and char != '>':
+                    running = False
+                    error = True
+                elif char == '>':
                     running = False
                 else:
                     subtitleadder.append(char)
@@ -58,6 +69,11 @@ def generateList():
     if contentadder != list(''):
         jsonvar.append({'type':'cont', 'content':''.join(contentadder)})
         contentadder = list('')
+
+    if error is True:
+        displayResult(False, code=-1)
+        inputText.focus_set()
+        return None
 
 
     return json.dumps(jsonvar)
@@ -82,7 +98,7 @@ def send():
     sender = mailFromEntry.get()
     recipient = mailToEntry.get()
 
-    sendMail('vitu0216@fridaskolan.se', '0402169114', subject, sender, recipient, html, text)
+    sendMail(selectedLogin.get(), subject, sender, recipient, html, text)#'vitu0216@fridaskolan.se'
 
 def addNewLines(text, maxChars):
     idx = 0
@@ -116,11 +132,14 @@ def displayResult(confirmed, text='', code=534):
             progText = addNewLines('The command has been aborted due to a server error. Not your fault.', 50)
         elif code is 555:
             progText = addNewLines('The server does not recognize the email address format, and delivery is not possible.', 50)
+        elif code is -1:
+            progText = addNewLines('Input field is invalid, check "< and >"', 50)
         else:
             progText = addNewLines('Could not send, please try again later', 50)
         resultLabel.configure(text=progText, fg='#ff0000', height=progText.count('\n')+1)
         progText = addNewLines(text, 50)
-        errorLabel.configure(text=("Error: "+progText), height=progText.count('\n')+1)
+        if text is not '':
+            passerrorLabel.configure(text=("Error: "+progText), height=progText.count('\n')+1)
     else:
         resultLabel.configure(text='Sent!', fg='#00ff00', height=1)
         errorLabel.grid_remove()
@@ -132,91 +151,119 @@ def resetResult(arg):
     errorLabel.update()
     resultLabel.update()
 
-def sendMail(username, password, subject, sender, recipient, html, text):
+def sendMail(username, subject, sender, recipient, html, text):
     sendBtn.update()
-    # Create message container - the correct MIME type is multipart/alternative.
-    msgRoot = MIMEMultipart('related')
-    # msg['Subject'] = subjectEntry.get()
-    msgRoot['Subject'] = subject
-    msgRoot['From'] = sender
-    msgRoot['To'] = recipient
-
-    msgAlternative = MIMEMultipart('alternative')
-    msgRoot.attach(msgAlternative)
-
-    # Create the body of the message (a plain-text and an HTML version).
-    # text = inputText.get(1, 'end')[:-1]
-    text = text
-    # entireHtml = open('customEntireMail.html', "r").read()
-    # html = html
-
-    # Record the MIME types of both parts - text/plain and text/html.
-    part1 = MIMEText(text, 'plain')
-    part2 = MIMEText(html, 'html')
-
-    # Attach parts into message container.
-    # According to RFC 2046, the last part of a multipart message, in this case
-    # the HTML message, is best and preferred.
-    msgAlternative.attach(part1)
-    msgAlternative.attach(part2)
-
-    # This example assumes the image is in the current directory
-    githubpng = open('github.png', 'rb')
-    githubimage = MIMEImage(githubpng.read())
-    githubpng.close()
-
-    googleplaypng = open('googleplay.png', 'rb')
-    googleplayimage = MIMEImage(googleplaypng.read())
-    googleplaypng.close()
-
-    instagrampng = open('instagram.png', 'rb')
-    instagramimage = MIMEImage(instagrampng.read())
-    instagrampng.close()
-
-    snapchatpng = open('snapchat.png', 'rb')
-    snapchatimage = MIMEImage(snapchatpng.read())
-    snapchatpng.close()
-
-    twitterpng = open('twitter.png', 'rb')
-    twitterimage = MIMEImage(twitterpng.read())
-    twitterpng.close()
-
-    # Define the image's ID as referenced above
-    githubimage.add_header('Content-ID', '<githubpng>')
-    msgRoot.attach(githubimage)
-    googleplayimage.add_header('Content-ID', '<googleplaypng>')
-    msgRoot.attach(googleplayimage)
-    instagramimage.add_header('Content-ID', '<instagrampng>')
-    msgRoot.attach(instagramimage)
-    snapchatimage.add_header('Content-ID', '<snapchatpng>')
-    msgRoot.attach(snapchatimage)
-    twitterimage.add_header('Content-ID', '<twitterpng>')
-    msgRoot.attach(twitterimage)
-
-    # # Send the message via local SMTP server.
-    # s = smtplib.SMTP('localhost')
-    # # sendmail function takes 3 arguments: sender's address, recipient's address
-    # # and message to send - here it is sent as one string.
-    # s.sendmail(mailFromEntry.get(), mailToEntry.get(), msg.as_string())
-    # s.quit()
+    # Get logins and then get data associated with username
     try:
-        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-        # server = smtplib.SMTP_SSL('send.one.com', 465)
-        server.ehlo()
-        # server.login('vigor.turujlija@gmail.com', 'mdorutrbqojgdxwn')
-        # server.login('vigor@elvigo.com', 'stov04are')
-        server.login(username, password)
-        server.sendmail(sender, recipient, msgRoot.as_string())
-        server.quit()
-        server.close()
-        displayResult(True)
-    except smtplib.SMTPException as e:
-        displayResult(False, text=str(e))
+        savedJson = open('logins.json', "r").read()
+        jsonvar = json.loads(savedJson)
+        loginElem = json.loads('{}')
+        for elem in jsonvar:
+            if elem['username'] == username:
+                loginElem = elem
+                break
+        if username != 'Login':
+            # Create message container - the correct MIME type is multipart/alternative.
+            msgRoot = MIMEMultipart('related')
+            # msg['Subject'] = subjectEntry.get()
+            msgRoot['Subject'] = subject
+            msgRoot['From'] = sender
+            msgRoot['To'] = recipient
 
-root = tk.Tk()
-root.title("Mail Styler")
+            msgAlternative = MIMEMultipart('alternative')
+            msgRoot.attach(msgAlternative)
+
+            # Create the body of the message (a plain-text and an HTML version).
+            # text = inputText.get(1, 'end')[:-1]
+            text = text
+            # entireHtml = open('customEntireMail.html', "r").read()
+            # html = html
+
+            # Record the MIME types of both parts - text/plain and text/html.
+            part1 = MIMEText(text, 'plain')
+            part2 = MIMEText(html, 'html')
+
+            # Attach parts into message container.
+            # According to RFC 2046, the last part of a multipart message, in this case
+            # the HTML message, is best and preferred.
+            msgAlternative.attach(part1)
+            msgAlternative.attach(part2)
+
+            # This example assumes the image is in the current directory
+            githubpng = open('github.png', 'rb')
+            githubimage = MIMEImage(githubpng.read())
+            githubpng.close()
+
+            googleplaypng = open('googleplay.png', 'rb')
+            googleplayimage = MIMEImage(googleplaypng.read())
+            googleplaypng.close()
+
+            instagrampng = open('instagram.png', 'rb')
+            instagramimage = MIMEImage(instagrampng.read())
+            instagrampng.close()
+
+            snapchatpng = open('snapchat.png', 'rb')
+            snapchatimage = MIMEImage(snapchatpng.read())
+            snapchatpng.close()
+
+            twitterpng = open('twitter.png', 'rb')
+            twitterimage = MIMEImage(twitterpng.read())
+            twitterpng.close()
+
+            # Define the image's ID as referenced above
+            githubimage.add_header('Content-ID', '<githubpng>')
+            msgRoot.attach(githubimage)
+            googleplayimage.add_header('Content-ID', '<googleplaypng>')
+            msgRoot.attach(googleplayimage)
+            instagramimage.add_header('Content-ID', '<instagrampng>')
+            msgRoot.attach(instagramimage)
+            snapchatimage.add_header('Content-ID', '<snapchatpng>')
+            msgRoot.attach(snapchatimage)
+            twitterimage.add_header('Content-ID', '<twitterpng>')
+            msgRoot.attach(twitterimage)
+
+            # # Send the message via local SMTP server.
+            # s = smtplib.SMTP('localhost')
+            # # sendmail function takes 3 arguments: sender's address, recipient's address
+            # # and message to send - here it is sent as one string.
+            # s.sendmail(mailFromEntry.get(), mailToEntry.get(), msg.as_string())
+            # s.quit()
+            try:
+                server = smtplib.SMTP_SSL(loginElem['server'], 465) #'smtp.gmail.com'
+                # server = smtplib.SMTP_SSL('send.one.com', 465)
+                server.ehlo()
+                # server.login('vigor.turujlija@gmail.com', 'mdorutrbqojgdxwn')
+                # server.login('vigor@elvigo.com', 'stov04are')
+                server.login(loginElem['username'], loginElem['password'])
+                server.sendmail(sender, recipient, msgRoot.as_string())
+                server.quit()
+                server.close()
+                displayResult(True)
+            except smtplib.SMTPException as e:
+                displayResult(False, text=str(e))
+        else:
+            print("Please select or add login item")
+    except Exception as e:
+        print("Please select or add login item")
 
 rowCount=0
+
+frame = tk.Frame(root)
+
+selectedLogin.set('Login')
+savedJson = open('logins.json', "r").read()
+jsonvar = json.loads(savedJson)
+choices = {'Login'}
+for elem in jsonvar:
+    choices.add(elem['username'])
+dropDown = ttk.OptionMenu(frame, selectedLogin, *choices)
+dropDown.grid(row=0, column=0, sticky='e')#.grid(padx=10, pady=10, sticky='w', row=rowCount)
+
+ttk.Button(frame, text='manage').grid(row=0, column=1, sticky='w')#.grid(row=rowCount, sticky='w')
+
+frame.grid(row=rowCount, column=0, columnspan=2, sticky='w')
+
+rowCount += 1
 
 tk.Label(root, height=1, width=5, text='From', anchor='w').grid(column=0, row=rowCount, padx=10)
 mailFromEntry = tk.Entry(root, width=35)
